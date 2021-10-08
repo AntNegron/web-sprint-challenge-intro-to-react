dc9cfd96a140d515c5b99d6a9bb6b1814615c278
AntNegron <anthonyanegron@gmail.com>
2021-10-08 15:47:16 -0400

    added styling to Character component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import './App.css';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 import Character from './components/Character';
 import {BASE_URL} from './components/Constants';
 
-const StyledHeader = styled.h1`
-  font-size: 4em;
-`;
-
-const StyledBackground = styled.div`
-  background-image: './images/sw-bg.jpg';
-  background-size: cover;
-`;
 
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-    const [CharacterData, getCharacterData] = useState([]);
+    const [characterData, getCharacterData] = useState([]);
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
@@ -37,7 +29,7 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
-      <Character characters ={CharacterData}/>
+      <Character characters ={characterData}/>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -3,19 +3,23 @@ import React from "react";
 import styled from "styled-components";
 
 const CharacterCard = styled.div`
-    display: flex;
+    display: block;
     margin: 0 auto;
     padding: 5px;
+    width: 50%;
+    background-color:#7D7C84;
+    opacity: 85%;
 `;
 
 const StyledName = styled.h1`
     font-size: 2.5em;
-    text-decoration: 3px underline #14f518;
+    border: 3px solid #14f518;
 `;
 
-const StyledH2 = styled.h1`
+const StyledH2 = styled.h2`
     font-size: 1em;
     text-decoration: 3px underline #DE8F6E;
+    color: #ffffff;
 `;
 
 const Character =((props) => {
@@ -27,7 +31,7 @@ const Character =((props) => {
                 return (
                     <CharacterCard key = {index}>
                         <StyledName>{resp.name}</StyledName>
-                        <StyledH2>D.O.B.: {resp.birth_year}</StyledH2>
+                        <StyledH2>D.O.B: {resp.birth_year}</StyledH2>
                         <StyledH2>Gender: {resp.gender}</StyledH2>
                         <StyledH2>Mass: {resp.mass}</StyledH2>
 
